badcb49f3a74d4b391259d73d5edae1a6944b089
cupton <cupton@sfu.ca>
2023-04-01 16:32:46 -0700

    partially implemented, beatmap files copying

--- beaglehero-Linux/beatmap.c
+++ beaglehero-Linux/beatmap.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "include/memoryShare.h"
 #include "include/sharedInputStruct.h"
+
+#define READ_BUFFER_SIZE 100
 
 static bool beatmapLoaded = false;
 
@@ -19,10 +22,26 @@ void Beatmap_init(void)
     pPruBase = PRU_getPruMmapAddr();
 	pBeatmap = PRUSHARED_MEM_FROM_BASE(pPruBase);
     
-    pBeatmap->totalNotes = 1;
+    FILE *fileToRead = fopen("beatmaps/test.csv", "r");
+    char buffer[READ_BUFFER_SIZE];
 
-    pBeatmap->notes[0].input = 0x30;
-    pBeatmap->notes[0].timestamp = 3000;
+    if (fileToRead == NULL) {
+        printf("ERROR OPENING %s.", "test.csv");
+        exit(1);
+    }
+
+    while (fgets(buffer, READ_BUFFER_SIZE, fileToRead) != NULL) {
+        printf("%s\n", buffer);
+        
+        pBeatmap->totalNotes =+ 1;
+    }
+
+    fclose(fileToRead);
+
+    // pBeatmap->totalNotes = 1;
+
+    // pBeatmap->notes[0].input = 0x30;
+    // pBeatmap->notes[0].timestamp = 3000;
 }
 
 void Beatmap_cleanup(void)
