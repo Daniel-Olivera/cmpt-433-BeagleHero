0e2cdb47398c0fbe379b511ca30d150596710992
dolivera <daniel_olivera@sfu.ca>
2023-04-02 02:09:36 -0700

    Added Freeplay mode

--- beaglehero-Linux/guitar.c
+++ beaglehero-Linux/guitar.c
@@ -79,8 +79,9 @@ static void *guitarThreadMain(void *args)
 					input = 0x00;
 					input = handle_event(wiimotes[0]);
 					//if strum or start were hit
-					if((input & STRUM_MASK) != 0
-						|| (input & START_MASK) != 0) {
+					if(    (input & STRUM_MASK) != 0
+						|| (input & START_MASK) != 0
+						|| (input & MINUS_MASK) != 0) {
 						pSharedInput->input = input;
 						pSharedInput->newInput = true;
 					}
@@ -183,6 +184,7 @@ static unsigned char handle_event(struct wiimote_t* wm)
 		}
 		if (IS_PRESSED(gh3, GUITAR_HERO_3_BUTTON_MINUS)) {
 			printf("Guitar: Minus pressed\n");
+			input |= MINUS_MASK;
 		}
 
 
--- beaglehero-Linux/main.c
+++ beaglehero-Linux/main.c
@@ -1,14 +1,16 @@
 #include <stdbool.h>
-
+#include <stdio.h>
 #include "include/guitar.h"
 #include "include/beatmap.h"
+#include "include/freeplay.h"
 
 int main(int argc, char *args[])
 {
     Beatmap_init();
     Guitar_init();
 
-    while(true){}
+    // while(true){}
+    freeplay_start();
 
     Guitar_cleanup();
     Beatmap_cleanup();
