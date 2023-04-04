11cff4afe8e5e5aa1e7d6fe8897f8197c5297599
dtjokroa <dtjokroa@sfu.ca>
2023-04-04 05:02:18 -0700

    Added logic to have LED read from bitmap and response to when a note is hit

--- beaglehero-LED/led-PRU.c
+++ beaglehero-LED/led-PRU.c
@@ -6,30 +6,115 @@
 #include "../beaglehero-Linux/include/sharedStructs.h"
 
 
-#define STR_LEN         64      // # LEDs in our string
+volatile register uint32_t __R30;
+
+#define STR_LEN         64      // # LEDs in our Matrix
+
+// Cycle timings taken from Dr. Brian Fraser's LED RGBW demo
+// ---------------------------------------------------------
 #define oneCyclesOn     700/5   // Stay on 700ns
 #define oneCyclesOff    800/5
 #define zeroCyclesOn    350/5
 #define zeroCyclesOff   600/5
-#define resetCycles     60000/5 // Must be at least 50u, use 60u
+#define cleanupCycles     60000/5 // Must be at least 50u, use 60u
 
-#define delayCycle_ms 30000000
+// Delay is calculated by beat intervals vs LED spacing
+// (beatATimeStamp - beatBTimeStamp)/number of desired LED spacing
+#define cyclesPerLoop 200000000/5
+
+#define updateCycle 1
+
+
+
 
 #define Color(r,g,b) ((r&0xFF) << 8 | (g&0xFF) << 16 | (b&0xff))
-
 #define RED (Color(63,0,0))
 #define GREEN (Color(0,63,0))
 #define BLUE (Color(0,0,63))
 #define ORANGE (Color(63,12,0))
 #define YELLOW (Color(63,37,0))
 
-#define DATA_PIN 15       // Bit number to output on
+// GPIO Input: P8_11 = pru0_pru_r30_15
+// Drives the LED Matrix
+#define DATA_PIN 15       
 
-#define PRU_SHAREDMEM 0x10000 
+volatile sharedResponseStruct_t *pSharedResponse =
+    (volatile void *)PRU_SHAREDMEM;
 
-#define NOTE_WINDOW_MS 30
+volatile beatmap_t *pBeatmap = 
+    (volatile void *)PRU_SHAREDMEM;
 
-volatile register uint32_t __R30;
+uint32_t newNote_index = 0;
+
+uint32_t targetNote_index = 0;
+
+uint64_t timeSinceStart = 0;
+
+static void turnBitOn(void);
+
+static void turnBitOff(void);
+
+static void setLED(uint32_t colour);
+
+static void setLedByArray(unsigned int* array);
+
+static void turnAllOff(void);
+
+static unsigned int* initLedGrid();
+
+static void cleanUp(unsigned int* grid);
+
+void main(void)
+{
+    // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
+    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+    unsigned int* grid = initLedGrid();
+    pSharedResponse->noteHit = false;
+    pSharedResponse->newResponseCombo = false;
+    pSharedResponse->songStarting = false;
+    pSharedResponse->noteAttemptedLED = false;
+    pBeatmap->totalNotes = 0;
+    
+
+    int flag = 1;
+    uint32_t nextNoteTimeStamp= pBeatmap->notes[currentNote].timestamp;
+    uint32_t timeStampOffset = pBeatmap->notes[currentNote].timestamp;
+    while(true){
+
+        if(!pSharedResponse->songStarting) continue;
+        
+        setLedByArray(grid);
+        for(int i = 0; i < 5; i++){
+            grid[i] <<= 1;
+        }
+
+        // If the timeSinceStart + Offset is larger than or equal to the timestamp of the next note,
+        // get the input bit of the newNote and set the bit in the corresponding row based on the input bit colour
+        if((timeSinceStart/1000) + timeStampOffset >= nextNoteTimeStamp){
+            char newNote = pBeatmap->notes.[newNote_index]->input;
+            grid[(int)newNote] += 1;
+            newNote_index += 1;
+            nextNoteTimeStamp = pBeatmap->notes.[newNote_index]->timestamp;
+            setLedByArray(grid);
+        }
+
+        // If note was hit, turn off the MSB of the binary literal and update the grid and continue with no delay
+        if(pSharedResponse->noteAttemptedLED){
+            char targetNote = pBeatmap->notes.[targetNote_index]->input;
+            grid[(int)newNote] &= ~(1 << 7);
+            setLedByArray(grid);
+            pSharedResponse->noteAttemptedLED = false;
+            __delay_cycles(1)
+        }
+
+        timeSinceStart += cyclesPerLoop;
+        __delay_cycles(cyclesPerLoop);
+    }
+
+    // turnAllOff();
+    cleanUp(grid);
+}
 
 void turnBitOn(void){
     __R30 |= 0x1<<DATA_PIN;      // Set the GPIO pin to 1
@@ -56,7 +141,7 @@ void setLED(uint32_t colour){
 }
 
 void setLedByArray(unsigned int* array){
-    uint32_t colours[5] = {GREEN,RED,YELLOW,BLUE,ORANGE};
+    uint32_t colours[5] = {ORANGE,BLUE,YELLOW,RED,GREEN};
     for(int i= 0; i < 8; i++){
         for(int j = 7; j >= 0; j--){
             if(array[i] & (0b1 << j)){
@@ -72,22 +157,6 @@ void setLedByArray(unsigned int* array){
     return;
 } 
 
-void shiftBinary(unsigned int* binary){
-    *binary <<= 1;
-    return;
-}
-
-void turnOnAll(void){
-    for(int j = 0; j < 64; j++) {
-        for(int i = 23; i >= 0; i--) {
-            if(ORANGE & ((uint32_t)0x1 << i)) {
-                turnBitOn();
-            }else{
-                turnBitOff();
-            }
-        }
-    } 
-}
 
 void turnAllOff(void){
     for(int j = 0; j < 64; j++) {
@@ -107,14 +176,9 @@ unsigned int* initLedGrid(){
     grid[5] = 0b00000000;
     grid[6] = 0b00000000;
     grid[7] = 0b00000000;
-
     return grid;
 }
 
-unsigned int addToBinary(unsigned int* binary)
-{
-    *binary = *binary + 1;
-}
 
 void cleanUp(unsigned int* grid)
 {
@@ -122,39 +186,7 @@ void cleanUp(unsigned int* grid)
 
     // Send Reset
     __R30 &= ~(0x1<<DATA_PIN);   // Clear the GPIO pin
-    __delay_cycles(resetCycles);
+    __delay_cycles(cleanupCycles);
     __halt();
-
-}
-
-void main(void)
-{
-    // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
-    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-    __delay_cycles(resetCycles);
-    unsigned int* grid = initLedGrid();
-
-    setLedByArray(grid);
-    int flag = 1;
-    while(true){
-        setLedByArray(grid);
-        for(int i = 0; i < 5; i++){
-            shiftBinary(&grid[i]);
-        }
-        for(int i = 0; i < 5; i++){
-            if(flag == 8){
-                addToBinary(&grid[i]);
-                flag = 1;
-            }
-            else{
-                flag++;
-            }
-        }
-        __delay_cycles(delayCycle_ms);
-    }
-
-    // turnAllOff();
-    cleanUp(grid);
 
 }
